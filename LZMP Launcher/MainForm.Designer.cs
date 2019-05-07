79653808d9bd3547ccc0af57cf7f7a3344fa8f97
xu13989 <xu13989@163.com>
2019-05-07 19:27:04 +0800

    Revert "Revert some settings"
    This reverts commit 20b80b186aaa73ddc18b73b578e4d7d565b21b55.

--- LZMP Launcher/MainForm.Designer.cs
+++ LZMP Launcher/MainForm.Designer.cs
@@ -53,9 +53,10 @@
             this.SmallTitle.AutoSize = true;
             this.SmallTitle.Font = new System.Drawing.Font("Segoe UI", 24F);
             this.SmallTitle.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.SmallTitle.Location = new System.Drawing.Point(25, 26);
+            this.SmallTitle.Location = new System.Drawing.Point(38, 42);
+            this.SmallTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.SmallTitle.Name = "SmallTitle";
-            this.SmallTitle.Size = new System.Drawing.Size(176, 54);
+            this.SmallTitle.Size = new System.Drawing.Size(283, 86);
             this.SmallTitle.TabIndex = 0;
             this.SmallTitle.Text = "ExMatics";
             this.SmallTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
@@ -65,9 +66,10 @@
             this.BigTitle.AutoSize = true;
             this.BigTitle.Font = new System.Drawing.Font("Segoe UI", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BigTitle.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.BigTitle.Location = new System.Drawing.Point(23, 80);
+            this.BigTitle.Location = new System.Drawing.Point(34, 128);
+            this.BigTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.BigTitle.Name = "BigTitle";
-            this.BigTitle.Size = new System.Drawing.Size(181, 72);
+            this.BigTitle.Size = new System.Drawing.Size(288, 114);
             this.BigTitle.TabIndex = 1;
             this.BigTitle.Text = "LZMP ";
             this.BigTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
@@ -79,10 +81,10 @@
             this.MainTree.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.MainTree.ForeColor = System.Drawing.Color.White;
             this.MainTree.LineColor = System.Drawing.Color.Gainsboro;
-            this.MainTree.Location = new System.Drawing.Point(34, 334);
-            this.MainTree.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MainTree.Location = new System.Drawing.Point(51, 534);
+            this.MainTree.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.MainTree.Name = "MainTree";
-            this.MainTree.Size = new System.Drawing.Size(524, 431);
+            this.MainTree.Size = new System.Drawing.Size(784, 687);
             this.MainTree.TabIndex = 3;
             this.MainTree.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.MainTree_AfterCheck);
             // 
@@ -91,10 +93,10 @@
             this.LaunchClient.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.LaunchClient.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LaunchClient.ForeColor = System.Drawing.Color.White;
-            this.LaunchClient.Location = new System.Drawing.Point(34, 781);
-            this.LaunchClient.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.LaunchClient.Location = new System.Drawing.Point(51, 1250);
+            this.LaunchClient.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.LaunchClient.Name = "LaunchClient";
-            this.LaunchClient.Size = new System.Drawing.Size(168, 69);
+            this.LaunchClient.Size = new System.Drawing.Size(252, 110);
             this.LaunchClient.TabIndex = 4;
             this.LaunchClient.Text = "Client";
             this.MainToolTip.SetToolTip(this.LaunchClient, "This will automaticly apply the current set. ");
@@ -106,10 +108,10 @@
             this.Apply.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Apply.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Apply.ForeColor = System.Drawing.Color.White;
-            this.Apply.Location = new System.Drawing.Point(390, 781);
-            this.Apply.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Apply.Location = new System.Drawing.Point(585, 1250);
+            this.Apply.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.Apply.Name = "Apply";
-            this.Apply.Size = new System.Drawing.Size(168, 69);
+            this.Apply.Size = new System.Drawing.Size(252, 110);
             this.Apply.TabIndex = 5;
             this.Apply.Text = "Apply";
             this.Apply.UseVisualStyleBackColor = true;
@@ -120,10 +122,10 @@
             this.CheckAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.CheckAll.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CheckAll.ForeColor = System.Drawing.Color.White;
-            this.CheckAll.Location = new System.Drawing.Point(34, 251);
-            this.CheckAll.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.CheckAll.Location = new System.Drawing.Point(51, 402);
+            this.CheckAll.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.CheckAll.Name = "CheckAll";
-            this.CheckAll.Size = new System.Drawing.Size(245, 69);
+            this.CheckAll.Size = new System.Drawing.Size(368, 110);
             this.CheckAll.TabIndex = 6;
             this.CheckAll.Text = "Check All";
             this.CheckAll.UseVisualStyleBackColor = true;
@@ -135,10 +137,10 @@
             this.ExitForm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ExitForm.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ExitForm.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ExitForm.Location = new System.Drawing.Point(510, 22);
-            this.ExitForm.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ExitForm.Location = new System.Drawing.Point(765, 35);
+            this.ExitForm.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.ExitForm.Name = "ExitForm";
-            this.ExitForm.Size = new System.Drawing.Size(47, 54);
+            this.ExitForm.Size = new System.Drawing.Size(70, 86);
             this.ExitForm.TabIndex = 7;
             this.ExitForm.Text = "X";
             this.MainToolTip.SetToolTip(this.ExitForm, "Closing the form won\'t apply the sets. ");
@@ -150,10 +152,10 @@
             this.CancelAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.CancelAll.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CancelAll.ForeColor = System.Drawing.Color.White;
-            this.CancelAll.Location = new System.Drawing.Point(313, 251);
-            this.CancelAll.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.CancelAll.Location = new System.Drawing.Point(470, 402);
+            this.CancelAll.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.CancelAll.Name = "CancelAll";
-            this.CancelAll.Size = new System.Drawing.Size(245, 69);
+            this.CancelAll.Size = new System.Drawing.Size(368, 110);
             this.CancelAll.TabIndex = 8;
             this.CancelAll.Text = "Cancel All";
             this.CancelAll.UseVisualStyleBackColor = true;
@@ -164,10 +166,10 @@
             this.ReadSet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ReadSet.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ReadSet.ForeColor = System.Drawing.Color.White;
-            this.ReadSet.Location = new System.Drawing.Point(313, 170);
-            this.ReadSet.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ReadSet.Location = new System.Drawing.Point(470, 272);
+            this.ReadSet.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.ReadSet.Name = "ReadSet";
-            this.ReadSet.Size = new System.Drawing.Size(245, 69);
+            this.ReadSet.Size = new System.Drawing.Size(368, 110);
             this.ReadSet.TabIndex = 10;
             this.ReadSet.Text = "Read Set";
             this.MainToolTip.SetToolTip(this.ReadSet, "This will override the current set. ");
@@ -179,10 +181,10 @@
             this.SaveSet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.SaveSet.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SaveSet.ForeColor = System.Drawing.Color.White;
-            this.SaveSet.Location = new System.Drawing.Point(34, 170);
-            this.SaveSet.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.SaveSet.Location = new System.Drawing.Point(51, 272);
+            this.SaveSet.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.SaveSet.Name = "SaveSet";
-            this.SaveSet.Size = new System.Drawing.Size(245, 69);
+            this.SaveSet.Size = new System.Drawing.Size(368, 110);
             this.SaveSet.TabIndex = 9;
             this.SaveSet.Text = "Save Set";
             this.MainToolTip.SetToolTip(this.SaveSet, "This will automaticly apply the set. ");
@@ -194,10 +196,10 @@
             this.LaunchServer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.LaunchServer.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LaunchServer.ForeColor = System.Drawing.Color.White;
-            this.LaunchServer.Location = new System.Drawing.Point(212, 781);
-            this.LaunchServer.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.LaunchServer.Location = new System.Drawing.Point(318, 1250);
+            this.LaunchServer.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.LaunchServer.Name = "LaunchServer";
-            this.LaunchServer.Size = new System.Drawing.Size(168, 69);
+            this.LaunchServer.Size = new System.Drawing.Size(252, 110);
             this.LaunchServer.TabIndex = 14;
             this.LaunchServer.Text = "Server";
             this.MainToolTip.SetToolTip(this.LaunchServer, "This will automaticly apply the current set. ");
@@ -216,27 +218,29 @@
             // 
             this.PlaceHolder.AutoSize = true;
             this.PlaceHolder.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
-            this.PlaceHolder.Location = new System.Drawing.Point(535, 861);
+            this.PlaceHolder.Location = new System.Drawing.Point(802, 1378);
+            this.PlaceHolder.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.PlaceHolder.Name = "PlaceHolder";
-            this.PlaceHolder.Size = new System.Drawing.Size(23, 15);
+            this.PlaceHolder.Size = new System.Drawing.Size(34, 24);
             this.PlaceHolder.TabIndex = 13;
             this.PlaceHolder.Text = "PH";
             // 
             // MainProgressBar
             // 
-            this.MainProgressBar.Location = new System.Drawing.Point(34, 100);
+            this.MainProgressBar.Location = new System.Drawing.Point(51, 160);
+            this.MainProgressBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MainProgressBar.Maximum = 2000;
             this.MainProgressBar.Name = "MainProgressBar";
-            this.MainProgressBar.Size = new System.Drawing.Size(524, 47);
+            this.MainProgressBar.Size = new System.Drawing.Size(786, 75);
             this.MainProgressBar.TabIndex = 15;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
-            this.ClientSize = new System.Drawing.Size(591, 874);
+            this.ClientSize = new System.Drawing.Size(886, 1398);
             this.Controls.Add(this.MainProgressBar);
             this.Controls.Add(this.LaunchServer);
             this.Controls.Add(this.PlaceHolder);
@@ -252,7 +256,7 @@
             this.Controls.Add(this.SmallTitle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
