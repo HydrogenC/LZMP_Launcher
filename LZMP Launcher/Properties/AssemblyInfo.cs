da989ecae4bd377a54a06a9e776ebf9f8c5bcfef
xu13989 <xu13989@163.com>
2019-06-22 16:28:40 +0800

    Bump application version

--- LZMP Launcher/Properties/AssemblyInfo.cs
+++ LZMP Launcher/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ExMatics")]
 [assembly: AssemblyProduct("LZMP Launcher")]
-[assembly: AssemblyCopyright("Copyright ExMatics™ LZMC©  2018")]
+[assembly: AssemblyCopyright("Copyright ExMatics™ LZMC©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.5")]
-[assembly: AssemblyFileVersion("2.0.5")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
