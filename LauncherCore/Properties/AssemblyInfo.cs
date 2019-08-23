679da0e9796ed64b557e37e12174189f0ec3e559
YC Xu <x13989@126.com>
2019-08-23 18:01:10 +0800

    Improve structure.

--- LauncherCore/Properties/AssemblyInfo.cs
+++ LauncherCore/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("SharpZipLib")]
+[assembly: AssemblyTitle("LauncherCore")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharpZipLib")]
+[assembly: AssemblyProduct("LauncherCore")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("4345374e-ebd4-48dd-b875-62700fbcc60c")]
+[assembly: Guid("9163e24d-ed11-4328-8e7e-a40322492968")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
--- LibNBT/Readme.txt
+++
@@ -1,6 +0,0 @@
-The api is pretty obvious, but here's a head start.
-
-If you want to load from a file use the static method TagCompound.ReadFromFile(filename).
-If you want to safe to a file use the instance method TagCompound.WriteToFile(filename).
-
-Also for streams use TagCompound.Write and AbstractTag.Read.
