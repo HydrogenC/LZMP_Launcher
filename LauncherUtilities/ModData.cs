5b465bb511609d68b483d39c7a30df991bd9c839
ExMatics HydrogenC <33123710+HydrogenC@users.noreply.github.com>
2023-01-15 21:55:59 +0800

    Preparation for auto update detection

---
+++ LauncherUtilities/ModData.cs
@@ -0,0 +1,71 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tomlyn;
+using Tomlyn.Model;
+
+namespace LauncherUtilities
+{
+    class ModData
+    {
+        public static string TempFolder = Path.GetTempPath();
+
+        public string Version { get; set; }
+        public string DisplayName { get; set; }
+        public string ModId { get; set; }
+        public string DisplayUrl { get; set; }
+
+        public ModData(string jarPath)
+        {
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempDirectory);
+
+            FastZip fastZip = new FastZip();
+            fastZip.ExtractZip(jarPath, tempDirectory, @"+\.toml$;+\.MF$");
+
+            string tomlPath = Path.Combine(tempDirectory, "META-INF\\mods.toml");
+            if (!File.Exists(tomlPath))
+            {
+                throw new FileNotFoundException("No mods.toml found! ");
+            }
+
+            var tomlModel = Toml.ToModel(File.ReadAllText(tomlPath));
+            var modsTable = (TomlTableArray)tomlModel["mods"]!;
+
+            DisplayName = GetValueOrNull(modsTable[0], "displayName");
+            Version = GetValueOrNull(modsTable[0], "version");
+            ModId = GetValueOrNull(modsTable[0], "modId");
+            DisplayUrl = GetValueOrNull(modsTable[0], "displayURL");
+
+            if (Version == "${file.jarVersion}")
+            {
+                string manifestPath = Path.Combine(tempDirectory, "META-INF\\MANIFEST.MF");
+                foreach (var item in File.ReadAllLines(manifestPath))
+                {
+                    string[] parts = item.Split(':');
+                    if (parts[0].Trim() == "Implementation-Version")
+                    {
+                        Version = parts[1].Trim();
+                        break;
+                    }
+                }
+            }
+        }
+
+        static string GetValueOrNull(TomlTable table, string tomlKey)
+        {
+            if (table.ContainsKey(tomlKey))
+            {
+                return (string)table[tomlKey];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- LauncherUtilities/Program.cs
+++ LauncherUtilities/Program.cs
@@ -3,12 +3,31 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace LauncherUtilities
 {
     class Program
     {
+        const string API_KEY = "$2a$10$1HxEu67mkjMNTvOMzhL4pOvCXRpLA.S.EUMR2gkBt9LSNd/p6eyMS";
+        public static HttpClient Client
+        {
+            get; set;
+        } = new HttpClient();
+
+        static string GetOptionalModPath(string filename)
+        {
+            return Path.Combine(SharedData.ResourcePath, $"{filename}.jar");
+        }
+
+        static string RequestFiles(string modName)
+        {
+            var res = Client.GetAsync($"https://api.curseforge.com/v1/mods/{modName}/files").Result;
+            return res.Content.ReadAsStringAsync().Result;
+        }
+
         static void Main(string[] args)
         {
             SharedData.DisplayMessage = (string content, string caption, MessageType type) =>
@@ -22,13 +41,17 @@ namespace LauncherUtilities
                 return MessageResult.OK;
             };
 
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            Client.DefaultRequestHeaders.Add("x-api-key", API_KEY);
+
             Scanner.ScanConfig(SharedData.WorkingPath + "\\BasicSettings.xml");
             Console.WriteLine("Modpack version: " + SharedData.Version);
             Core.CheckInstallation();
             Core.CheckAvailability();
 
             Console.WriteLine("1. Clean up resources");
-            Console.WriteLine("2. Open curseforge pages of the mods");
+            Console.WriteLine("2. Check update of optional mods");
+            Console.WriteLine("3. Check update of builtin mods");
             switch (Console.ReadKey().Key)
             {
                 case ConsoleKey.D1:
@@ -38,30 +61,45 @@ namespace LauncherUtilities
                     break;
                 case ConsoleKey.D2:
                     Console.WriteLine('\n');
-                    var links = Scanner.ScanCurseforgeLinks(SharedData.WorkingPath + "\\BasicSettings.xml");
+                    var entries = Scanner.ScanCurseforgeLinks(SharedData.WorkingPath + "\\BasicSettings.xml");
                     int index = 1;
-                    foreach (var i in links)
+                    foreach (var i in entries)
                     {
                         Console.WriteLine("{0}. {1}", index, i.Item1);
+                        try
+                        {
+                            var data = new ModData(GetOptionalModPath(i.Item1));
+                            if (data.Version != null)
+                            {
+                                Console.WriteLine("Version: {0}", data.Version);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
                         index++;
                     }
-                    Console.WriteLine("Which link to start from? ");
-                    int.TryParse(Console.ReadLine(), out index);
+
+                    Console.WriteLine(RequestFiles("293426"));
+                    /*
                     if (index > 0)
                     {
                         index--;
                     }
-                    for (int i = index; i < links.Length; i++)
+                    for (int i = index; i < entries.Length; i++)
                     {
                         Console.WriteLine("Press any key to continue...");
                         Console.ReadKey();
-                        Console.WriteLine("Current file: {0}", links[i].Item1);
-                        if (i != links.Length - 1)
+                        Console.WriteLine("Current file: {0}", entries[i].Item1);
+                        if (i != entries.Length - 1)
                         {
-                            Console.WriteLine("Pending file: {0}", links[i + 1].Item1);
+                            Console.WriteLine("Pending file: {0}", entries[i + 1].Item1);
                         }
-                        Process.Start("explorer.exe", $"https://www.curseforge.com/minecraft/mc-mods/{links[i].Item2}");
+                        Process.Start("explorer.exe", $"https://www.curseforge.com/minecraft/mc-mods/{entries[i].Item2}");
                     }
+                    */
                     break;
                 default:
                     break;
